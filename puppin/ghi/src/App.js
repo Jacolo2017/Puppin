e8005df7bf4c4f12b79731655164632547243a0b
Roger Wang <wangrogerc@gmail.com>
2022-07-11 13:06:52 -0700

    setup docker yaml basics with react and postgres. setup tailwind

--- puppin/ghi/src/App.js
+++ puppin/ghi/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 
 export default function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
+    <h1 className="text-3xl font-bold underline  bg-red-50">
       Hello world!
     </h1>
   )

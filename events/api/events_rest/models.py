1689bc02160cc657ba3d99c83325f2b4f389076b
Roger Wang <wangrogerc@gmail.com>
2022-07-12 21:58:50 -0700

    source tree test

--- events/api/events_rest/models.py
+++ events/api/events_rest/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class DogVO(models.Model):
+    name = models.CharField(max_length=100)
+
+
+class PersonVO(models.Model):
+    name =  models.CharField(max_length=100)
+    email = models.EmailField()
+    dog = models.ForeignKey("DogVO", related_name="People", on_delete=models.CASCADE)
+
+class Event(models.Model):
+    name = models.CharField(max_length=100)
+    location = models.CharField(max_length=100)
+
+
+
+# event project will poll person hosting or person expressing interest in an event. Then it will poll for the dog(s) that the person chooses to bring to event
+
+

7b5c079fe4c13fb98986b0b002d4b61d14ebea49
Roger Wang <wangrogerc@gmail.com>
2022-07-13 14:58:59 -0700

    fixed port issue

--- accounts/api/main.py
+++ accounts/api/main.py
@@ -1,5 +1,12 @@
 from fastapi import FastAPI
 
-from routers import nothin
+from routers import accounts
 
 app = FastAPI()
+
+app.include_router(accounts.router)
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
---
+++ accounts/api/routers/accounts.py
@@ -0,0 +1,11 @@
+from fastapi import APIRouter, Response, status
+from pydantic import BaseModel
+import psycopg
+
+router = APIRouter()
+
+
+# @router.get("/api/create-user/{user_id}")
+# def createAccount(user_id: int, username: str, password: str):
+    
+

61af603b25721bfc89f93ec9455c24acb2370bd5
Roger Wang <wangrogerc@gmail.com>
2022-07-14 12:33:39 -0700

    create schemas initialization file added

---
+++ data/postgres/init/02createschemas.sql
@@ -0,0 +1,17 @@
+\connect puppin
+
+DROP TABLE if exists public.accounts;
+
+CREATE TABLE public.accounts(
+    id serial NOT NULL PRIMARY KEY,
+    first_name character varying(30),
+    last_name character varying(30),
+    email character varying(50,)
+    username character varying(30),
+    password character varying(50),
+    
+    
+
+    
+
+)

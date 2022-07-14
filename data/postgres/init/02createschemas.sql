fcb7da026c233a912fb1b9be825f1aa699b5d6f6
Jack <jacolo2017@gmail.com>
2022-07-14 15:24:46 -0600

    events, reviews, and location tables ready for testing

--- data/postgres/init/02createschemas.sql
+++ data/postgres/init/02createschemas.sql
@@ -15,3 +15,61 @@ CREATE TABLE public.accounts(
     
 
 )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+DROP TABLE if exists public.events;
+
+CREATE TABLE public.events(
+    event_id serial NOT NULL PRIMARY KEY,
+    event_name character varying(100) NOT NULL,
+    event_location character varying(100) NOT NULL,
+    PRIMARY KEY (event_id),
+    FOREIGN KEY (location_nickname) REFERENCES public.location(location_nickname),
+    event_accounts character varying(30) NOT NULL,
+    PRIMARY KEY (event_id),
+    FOREIGN KEY (username) REFERENCES public.accounts(username), 
+    event_date DATE NOT NULL,
+    event_time TIME NOT NULL,
+)
+
+CREATE TABLE public.reviews(
+    review_id serial NOT NULL PRIMARY KEY,
+    reviewer_username character varying(30) NOT NULL,
+    PRIMARY KEY (review_id),
+    FOREIGN KEY (username) REFERENCES public.accounts(username),
+    review_event character varying(100) NOT NULL,
+    PRIMARY KEY (review_id),
+    FROEIGN KEY (event_name) REFERENCES public.events(event_name),
+    attendee_rating BOOLEAN NOT NULL,
+    review_description character varying(750) NOT NULL,
+)
+
+CREATE TABLE public.location(
+    location_id serial NOT NULL PRIMARY KEY,
+    location_nickname character varying(30) NOT NULL,
+    location_address character varying(100) NOT NULL,
+    location_street character varying(40) NOT NULL,
+    location_city character varying(30) NOT NULL,
+    location_state character varying(12),
+    location_zip int NOT NULL,
+)

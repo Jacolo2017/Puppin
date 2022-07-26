d39134f1cdc545ade717c82142c6850cce42e996
Cooper Edmondson <cooperedmondson0115@gmail.com>
2022-07-26 19:46:43 -0400

    working on auth solution

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -7,25 +7,24 @@ import SignUp from './components/SignUp';
 import Login from './components/Login';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Events from './components/Events';
-import { AuthProvider } from './auth/Authentication';
+import { useToken } from './auth/Authentication';
+
 export default function App() {
-  
+  const [ token, login, logout, signup] = useToken();
 
   return (
     <>
     <BrowserRouter>
-      <AuthProvider>
           <Routes>
             <Route path="" element={[<Navbar/>,<Hero/>, <About/>, <Developers/>, <Footer/> ]}/>
             <Route path='registration'>
-              <Route path="login" element={<Login/>}/>
-              <Route path="create" element={<SignUp />}/>
+              <Route path="login" element={<Login  login={login} token={token}/>}/>
+              <Route path="create" element={<SignUp signup={signup} token={token}/>}/>
             </Route>
             <Route path='event'>
               <Route path='home'element={<Events/>} />
             </Route>
           </Routes>
-          </AuthProvider>
     </BrowserRouter>
     </>
   )

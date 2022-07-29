420d54c56ae6e10f33789dab89b426720950f5f5
Roger  Wang <wangrogerc@gmail.com>
2022-07-29 18:09:34 +0000

    Revert "Merge branch 'eventsFormRoger'"
    This reverts commit 50098f2d9e91a58e6920614dd913bbae09a4b9d8

--- events/api/main.py
+++ events/api/main.py
@@ -24,16 +24,3 @@ app.add_middleware(
 
 app.include_router(events.router)
 app.include_router(reviews.router)
-
-origins = [
-    "http://localhost:3000",
-    os.environ.get("CORS_HOST", None),
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)

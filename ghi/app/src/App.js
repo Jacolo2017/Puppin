c2321804fd0068b5b4745e98e3ec87bc6c0c516f
Cooper Edmondson <cooperedmondson0115@gmail.com>
2022-07-30 03:02:29 -0400

    fixing issues

--- ghi/app/src/App.js
+++ ghi/app/src/App.js
@@ -6,24 +6,41 @@ import Footer from './components/Footer';
 import SignUp from './components/SignUp';
 import Login from './components/Login';
 import DogRegister from './components/DogRegister';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Events from './components/Events';
 import { useToken } from './auth/Authentication';
 import LoggedinNav from './components/LoggedinNav';
+
+import Profile from './components/Profile';
+
+
+
+
+
+
 export default function App() {
   const [ token, login, logout] = useToken();
-
+  if (!{token}) {
+    <Navigate to='localhost:3000'/>
+  }
   return (
     <>
     <BrowserRouter>
           <Routes>
-            <Route path="" element={[ <Navbar/>,<Hero/>, <About/>, <Developers/>, <Footer/> ]}/>
+            <Route path="" element={[ <Navbar/>,<Hero token={token}/>, <About/>, <Developers/>, <Footer/> ]}/>
             <Route path='registration'>
               <Route path="login" element={<Login  login={login} token={token}/>}/>
               <Route path="create" element={<SignUp token={token}/>}/>
             </Route>
             <Route path='event'>
+
+              <Route path='home'element={[<LoggedinNav logout={logout} token={token}/>, <Events/>]} />
+            </Route>
+            <Route path='profile'>
+              <Route path=''element={[<LoggedinNav logout={logout} token={token}/>, <Profile/>]} />
+              {/* <Route path = 'create'element = {<CreateEvent token={token}/>}/> */}
               <Route path='home'element={[<LoggedinNav/>, <Events/>]} />
+
             </Route>
           </Routes>
     </BrowserRouter>
--- ghi/app/src/components/Events.js
+++ ghi/app/src/components/Events.js
@@ -1,40 +1,52 @@
-import React from 'react'
+import React from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
+import { FreeMode } from 'swiper'
+import EventCard from './EventCard';
+import 'swiper/css';
+import 'swiper/css/free-mode';
+export default function Events(){
 
-const Events = () => {
-  return (
-    <div className='grid grid-cols-3 items-center gap-5 px-10  w-full h-screen'>
-      
-      {/* left  */}
-      <div className='bg-gray-200 w-[400px] rounded-lg px-4 py-4 shadow-xl items-center'>
-        <div>
-        <img className='w-[400px] rounded-md' src='https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F47%2F2021%2F04%2F20%2Fdogs-playing-at-dog-park-1210828938-2000.jpg'/>
-        </div>
-        <h1 className='font-semibold text-3xl text-center py-3'>header</h1>
-        <p className='font-gray-600 text-center text-lg py-4'>this is the body omg</p>
-        <h2 className='font-gray-600 text-center text-lg py-3'>footer</h2>
-      </div>
-      {/* middle */}
-      <div className='bg-gray-200 w-[400px] rounded-lg px-4 py-4 shadow-xl items-center'>
-        <div>
-        <img className='w-[400px] rounded-md' src='https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F47%2F2021%2F04%2F20%2Fdogs-playing-at-dog-park-1210828938-2000.jpg'/>
-        </div>
-        <h1 className='font-semibold text-3xl text-center py-3 items-center'>header</h1>
-        <p className='font-gray-600 text-center text-lg py-4'>this is the body omg</p>
-        <h2 className='font-gray-600 text-center text-lg py-3'>footer</h2>
-      </div>
-      {/* right */}
-      <div className='bg-gray-200 w-[400px] rounded-lg px-4 py-4 shadow-xl'>
-        <div>
-        <img className='w-[400px] rounded-md' src='https://imagesvc.meredithcorp.io/v3/mm/image?url=https%3A%2F%2Fstatic.onecms.io%2Fwp-content%2Fuploads%2Fsites%2F47%2F2021%2F04%2F20%2Fdogs-playing-at-dog-park-1210828938-2000.jpg'/>
-        </div>
-        <h1 className='font-semibold text-3xl text-center py-3'>header</h1>
-        <p className='font-gray-600 text-center text-lg py-4'>this is the body omg</p>
-        <h2 className='font-gray-600 text-center text-lg py-3'>footer</h2>
-      </div>
+  return(
+    <div className='w-screen py-20 ' id="about">
+        <div className='max-w-[1300px] mx-auto py-10 mt-1'>
+            <div className='text-center'>
+                <h2 className='text-2xl font-bold uppercase'>Events</h2>
+            </div>
+
+            <div className=' grid-flow-row gap-10 px-4 py-20 text-center'>
+              <Swiper
+              freeMode={true}
+              grabCursor={false}
+              modules={FreeMode}
+              className='mySwiper py-10'
+              slidesPerView={3}
+              spaceBetween={100}
+              >
+
+                <SwiperSlide className='pt-4 rounded-sm'>
+                  <EventCard className=''/>
+                </SwiperSlide>
+                <SwiperSlide className='pt-4 rounded-sm'>
+                  <EventCard className=''/>
+                </SwiperSlide>
+                <SwiperSlide className='pt-4 rounded-sm'>
+                  <EventCard className=''/>
+                </SwiperSlide>
+                <SwiperSlide className='pt-4 rounded-sm'>
+                  <EventCard className=''/>
+                </SwiperSlide>
+                <SwiperSlide className='pt-4 rounded-sm'>
+                  <EventCard className=''/>
+                </SwiperSlide>
+                <SwiperSlide className='pt-4 rounded-sm'>
+                  <EventCard className=''/>
+                </SwiperSlide>
+                
 
 
+              </Swiper>
+            </div>
+        </div>
     </div>
   )
 }
-
-export default Events

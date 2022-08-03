8cd4c6d107a97c92b66100c3be92f26511a1c516
Cooper Edmondson <cooperedmondson0115@gmail.com>
2022-08-03 01:46:17 -0400

    asdh

--- ghi/app/src/components/PublicProfile.js
+++ ghi/app/src/components/PublicProfile.js
@@ -3,6 +3,7 @@ import { useParams } from 'react-router-dom';
 
 export default function PublicProfile(){
     let params = useParams();
+    console.log(params);
     
     return (
         <div>{params.username}</div>

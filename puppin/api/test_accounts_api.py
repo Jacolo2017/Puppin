c4099a838adf48e70c80ac549e7d15b60a23eca0
Roger Wang <wangrogerc@gmail.com>
2022-08-11 09:48:24 -0700

    test test

--- puppin/api/test_accounts_api.py
+++ puppin/api/test_accounts_api.py
@@ -13,6 +13,8 @@ from fastapi import (
 )
 
 
+
+
 async def get_fake_account():
     return {1, "P", "A", "h", "K", "01-15-2001", "A", "A", "Male", "p", "I"}
 
@@ -48,25 +50,25 @@ class FakeAccountCreate(TestCase):
     #     "a",
     # ]
 
+async def common_parameters_acc(first_name: str, last_name: str, email: str, username: str, date_of_birth: str, city: str, state: str, gender: str, account_id: int, photo_url: str, about: str)
+    return {"first_name": first_name, "last_name": last_name, "email": email, "username": username, "date_of_birth": date_of_birth, "city": city, "state": state, "gender": gender, "account_id": account_id, "photo_url": photo_url, "about": about}
 
-class FakeAccountQuery(TestCase):
-    def get_account_from_username(self, username: str):
-        print("savage")
-        return {
-            "first_name": "rog2er",
-            "last_name": "rog2er",
-            "email": "ro",
-            "username": "ro333g2er",
-            "date_of_birth": "2021-08-03",
-            "city": "roger",
-            "state": "roger",
-            "gender": "roger",
-            "account_id": 1,
-            "photo_url": "231312312",
-            "about": "gangstuff",
-        }
+async def get_account_from_username(self, username: str):
+    return {
+        "first_name": "rog2er",
+        "last_name": "rog2er",
+        "email": "ro",
+        "username": "ro333g2er",
+        "date_of_birth": "2021-08-03",
+        "city": "roger",
+        "state": "roger",
+        "gender": "roger",
+        "account_id": 1,
+        "photo_url": "231312312",
+        "about": "gangstuff",
+    }
 
-
+app.dependency_overrides[common_parameter_acc] = get_account_from_username
 # async def get_fake_user(self, username: str):
 #     return {
 #         "id": 1,
@@ -77,20 +79,20 @@ class FakeAccountQuery(TestCase):
 
 # app.dependency_overrides[AccountQueries] = FakeAccountQuery
 
-# async def get_fake_account():
-#     return {
-#         "account_id": 1,
-#         "first_name": "P",
-#         "last_name": "A",
-#         "email": "h",
-#         "username": "K",
-#         "date_of_birth": "01-15-2001",
-#         "city": "A",
-#         "state": "A",
-#         "gender": "Male",
-#         "photo_url": "p",
-#         "about": "I"
-#     }
+async def get_fake_account():
+    return {
+        "account_id": 1,
+        "first_name": "P",
+        "last_name": "A",
+        "email": "h",
+        "username": "K",
+        "date_of_birth": "01-15-2001",
+        "city": "A",
+        "state": "A",
+        "gender": "Male",
+        "photo_url": "p",
+        "about": "I"
+    }
 
 # app.dependency_overrides[AccountQueries] = get_fake_account()
 # app.dependency_overrides[create_account] = create_fake_account
@@ -122,13 +124,12 @@ client = TestClient(app)
 
 
 # # this doesnt work in pipeline CI, commenting out for now
-# def test_get_user_200():
-#     app.dependency_overrides[AccountQueries] = FakeAccountQuery
-#     print(client)
-#     res = client.get("/api/accounts/by_username/K")
-#     print("HERE:", res)
-#     assert res.status_code == 200
-#     app.dependency_overrides = {}
+def test_get_user_200():
+    print(client)
+    res = client.get("/api/accounts/by_username/K")
+    print("HERE:", res)
+    assert res.status_code == 200
+    app.dependency_overrides = {}
 
 
 # create_fake_account(first_name="paul",

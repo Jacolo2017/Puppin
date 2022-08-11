2af7519f1cc9ea8abc770ad540092c79cb92ff2b
Roger Wang <wangrogerc@gmail.com>
2022-08-10 20:28:46 -0700

    test

--- puppin/api/main.py
+++ puppin/api/main.py
@@ -12,7 +12,7 @@ origins = [
     "http://purely-puptonic.gitlab.io",
     "http://puppin.herokuapp.com",
     "https://puppin.herokuapp.com",
-    os.environ.REACT_APP_PUPPIN_HOST,
+    os.environ.get("REACT_APP_PUPPIN_HOST", None),
     os.environ.CORS_HOST,
     os.environ.get("CORS_HOST", None),
     os.environ.get("PUBLIC_URL", None),

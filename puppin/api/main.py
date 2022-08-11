dd239d0058dd0934d7828de3864d3d26821f1c60
Roger Wang <wangrogerc@gmail.com>
2022-08-10 20:54:10 -0700

    cors host

--- puppin/api/main.py
+++ puppin/api/main.py
@@ -6,26 +6,39 @@ import os
 
 app = FastAPI()
 
-
 origins = [
     "http://localhost:3000",
-    "http://purely-puptonic.gitlab.io",
-    "http://puppin.herokuapp.com",
-    "https://puppin.herokuapp.com",
-    os.environ.get("REACT_APP_PUPPIN_HOST", None),
     os.environ.get("CORS_HOST", None),
-    os.environ.get("PUBLIC_URL", None),
 ]
-# os.environ.get("CORS_HOST", None),
-# os.environ["CORS_HOST"],
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=origins,
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
+
+# origins = [
+#     "http://localhost:3000",
+#     "http://purely-puptonic.gitlab.io",
+#     "http://puppin.herokuapp.com",
+#     "https://puppin.herokuapp.com",
+#     os.environ.get("REACT_APP_PUPPIN_HOST", None),
+
+#     os.environ.get("CORS_HOST", None),
+#     os.environ.get("PUBLIC_URL", None),
+# ]
+# # os.environ.get("CORS_HOST", None),
+# # os.environ["CORS_HOST"],
+
+# app.add_middleware(
+#     CORSMiddleware,
+#     allow_origins=origins,
+#     allow_credentials=True,
+#     allow_methods=["*"],
+#     allow_headers=["*"],
+# )
 
 
 app.include_router(accounts.router)
